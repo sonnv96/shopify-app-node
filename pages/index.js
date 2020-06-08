89a32d5b04378c3c886a79cdd278d0b1badb5dd3
SonNguyen <son.nguyen@spiraledge.com>
2020-06-08 15:58:21 +0700

    change app

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,7 @@
-import { EmptyState, Spinner, Layout, Page, Select, Button } from '@shopify/polaris';
-import Cookies from 'js-cookie';
-import Selector from './selector.js'
-import Window from './window.js'
-import Help from './help.js'
-
-class Index extends React.Component {
-  constructor(props){
-    super(props);
-  }
-
-  render() {
-    return (
-      <Page>
-        <Layout.Section>
-            <Selector />
-        </Layout.Section>
-        <Layout.Section>
-            <Window />
-        </Layout.Section>
-        <Layout.Section>
-            <Help />
-        </Layout.Section>
-      </Page>
-    );
-  };
-}
+const Index = () => (
+  <div>
+    <p>Sample app using React and Next.js</p>
+  </div>
+);
 
 export default Index;
